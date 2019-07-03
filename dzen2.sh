02a7f0aaa11bee348d04e51e2d5edfcdc3abc3bf
spookykidmm <spookykidmm@gmail.com>
2019-07-03 09:18:29 -0400

    Added wifi, brightness, and workspace name

--- dzen2.sh
+++ dzen2.sh
@@ -2,26 +2,41 @@
 gettime() {
 time=$(date +'%a %b %d %H:%M:%S')
 }
+
 getpower() {
 power=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage | tail -1 | awk '{print $2 }')
 }
 getvolume() {
 volume=$(pamixer --get-volume-human)
 }
+
+getbrightness() {
+	bright=$(light -G)
+}
+
 getmem() {
 memused=$(free -m | grep Mem: | tail -1 | awk '{print $3 }')
 memtotal=$(free -m | grep Mem: | tail -1 | awk '{print $2 }')
 }
+
 getupdates() {
 updates=$(checkupdates | wc -l)
 aur=$(aur repo -S -u -d revenge_repo | wc -l)
 }
-workspace() {
-top=$XDG_CURRENT_DESKTOP
-	if [ "$top" = "i3" ]
-	then
-space=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
-fi
+
+getworkspace() {
+space=$(wmctrl -ld | grep "*" | tail -1 | awk '{print $9 }')
+}
+
+updatecheck() {
+while true;
+      do getupdates
+      sleep 5
+      done
+}
+      
+getnetwork() {
+	wifi=$(iwgetid -r)
 }
 output() {
 	while true;
@@ -30,16 +45,10 @@ output() {
 	getpower
 	getvolume
 	getmem
-#	getupdates
-	workspace
-echo " $space   |   $time | $power | $volume | $memused / $memtotal"	
-	sleep 1
-	done
-}
-frick() {
-	while true;
-	do gettime
-	echo $time
+	getworkspace
+	getnetwork
+	getbrightness
+echo " $space   |   $time | $power | $volume | $bright | $memused / $memtotal | $wifi"	
 	sleep 1
 	done
 }
@@ -49,11 +58,5 @@ do output | dzen2
 sleep 1
 done
 }
-datebar() {
-	while true;
-	do frick | dzen2
-	done
-}
-#output
-#bar
+#updatecheck
 bar
